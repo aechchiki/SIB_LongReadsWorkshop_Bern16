afda032df93cacf0c2bda0ff8f6ae2281ad05426
Kamil Jaron <kamil.jaron@unil.ch>
2016-07-01 13:27:39 +0000

    fixed rescue scripts

--- scripts/2_RSII_Assembly.sh
+++ scripts/2_RSII_Assembly.sh
@@ -1,30 +1,42 @@
 #!/bin/bash
 
 #BSUB -L /bin/bash
-#BSUB -J Assembly_all
-#BSUB -q priority 
+#BSUB -J RSII_asm
+#BSUB -e RSII-assembly.err
+#BSUB -o RSII-assembly.out
+#BSUB -q dee-hugemem
 #BSUB -n 4
+#BSUB -R "rusage[mem=8000]"
+#BSUB -R "rusage[swp=20000]"
+#BSUB -v 20000000
+#BSUB -M 8000000
 
 ## prepare directory
-
 # define working directory for user
 work_dir=/scratch/beegfs/weekly/$USER
 # create working directory for MinION extraction
-mkdir -p $work_dir/lambda_RSII/miniasm
+mkdir -p $work_dir/lambda_RSII/asm_miniasm
 
-## load canu and miniasm
 module add UHTS/Assembler/canu/1.3;
-module add UHTS/Analysis/minimap/0.2.r124.dirty;
-module add UHTS/Analysis/miniasm/0.2.r137.dirty;
 
-## Canu assembly
+# take a subset of reads
+head -12000 $work_dir/RSII_LambdaSubreads.fastq > $work_dir/RSII_reads_subset.fastq
 
-canu -p lambda -d $work_dir/lambda_RSII/asm_canu/ genomeSize=48k -pacbio-raw $work_dir/lambda_RSII/RSII_LambdaSubreads.fastq -useGrid=false -maxThreads=4 &> canu_RSII_lambda.log
+# canu assemblu
+canu -p canu_RSII -d $work_dir/lambda_RSII/asm_canu/ genomeSize=48k -pacbio-raw $work_dir/lambda_RSII/RSII_reads_subset.fastq -useGrid=false -maxThreads=4
+
+cp $work_dir/lambda_RSII/asm_canu/canu_RSII.contigs.fasta $work_dir/lambda_RSII/canu_RSII_contigs.fa
 
 ## miniasm
 
-minimap -Sw5 -L100 -m0 -t4 $work_dir/lambda_RSII/RSII_LambdaSubreads.fastq $work_dir/lambda_RSII/RSII_LambdaSubreads.fastq | gzip -1 > $work_dir/lambda_RSII/miniasm/RSII_overlaps.paf.gz
+module purge
+module add UHTS/Analysis/minimap/0.2.r124.dirty;
+module add UHTS/Analysis/miniasm/0.2.r137.dirty;
 
-miniasm -f $work_dir/lambda_RSII/RSII_LambdaSubreads.fastq $work_dir/lambda_RSII/miniasm/RSII_overlaps.paf.gz > $work_dir/lambda_RSII/miniasm/lambda_miniasm_contigs.gfa
+minimap -Sw5 -L100 -m0 -t4 $work_dir/lambda_RSII/RSII_reads_subset.fastq $work_dir/lambda_RSII/RSII_reads_subset.fastq | gzip -1 > $work_dir/lambda_RSII/asm_miniasm/RSII_overlaps.paf.gz
 
-awk '/^S/{print ">"$2"\n"$3}' $work_dir/lambda_RSII/miniasm/lambda_miniasm_contigs.gfa | fold > $work_dir/lambda_RSII/miniasm/miniasm_RSII_contigs.fa
+miniasm -f $work_dir/lambda_RSII/RSII_reads_subset.fastq $work_dir/lambda_RSII/asm_miniasm/RSII_overlaps.paf.gz > $work_dir/lambda_RSII/asm_miniasm/lambda_miniasm_contigs.gfa
+
+awk '/^S/{print ">"$2"\n"$3}' $work_dir/lambda_RSII/asm_miniasm/lambda_miniasm_contigs.gfa | fold > $work_dir/lambda_RSII/miniasm_RSII_contigs.fa
+
+
