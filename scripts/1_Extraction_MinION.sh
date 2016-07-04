4dc9f89309205bd932729c851abc798355984ffd
Amina Echchiki <amina.echchiki@unil.ch>
2016-07-05 00:02:12 +0200

    updated rescue scripts part1

--- scripts/1_Extraction_MinION.sh
+++ scripts/1_Extraction_MinION.sh
@@ -5,9 +5,9 @@
 #BSUB -q priority 
 
 ## prepare directory
-
 # define working directory for user
 work_dir=/scratch/beegfs/weekly/$USER
+mkdir -p $work_dir
 # create working directory for MinION extraction
 mkdir -p $work_dir/lambda_minion/fast5/
 # create links to the raw sequencing files
@@ -17,10 +17,8 @@ ln -s /scratch/beegfs/monthly/SIB_long_read_workshop/MinION_lambda_reads/*.fast5
 module add UHTS/Analysis/poretools/0.5.1 
 
 ## extract reads 
-
 # convert all fast5 reads to fastq
 poretools fastq --type 2D $work_dir/lambda_minion/fast5/*.fast5 > $work_dir/lambda_minion/all_reads.fastq
-
 # get some stats on the raw data using poretools
 poretools stats $work_dir/lambda_minion/fast5/*.fast5 > $work_dir/lambda_minion/minion_all_reads_stats.txt
 
--- scripts/1_Extraction_RSII.sh
+++ scripts/1_Extraction_RSII.sh
@@ -5,9 +5,9 @@
 #BSUB -q priority 
 
 ## prepare directory
-
 # define working directory for user
 work_dir=/scratch/beegfs/weekly/$USER
+mkdir -p $work_dir
 # create working directory for MinION extraction
 mkdir -p $work_dir/lambda_RSII/raw_reads/
 # create links to the raw sequencing files
@@ -17,7 +17,6 @@ ln -s /scratch/beegfs/monthly/SIB_long_read_workshop/RSII_lambda_reads/*.h5 $wor
 module add UHTS/PacBio/pbh5tools/0.8.0
 
 ## extract reads 
-
 # convert all fast5 reads to fastq
 bash5tools.py $work_dir/lambda_RSII/raw_reads/m140715_200214_42182_c100661412550000001823125411271451_s1_p0.bas.h5 --outFilePrefix $work_dir/lambda_RSII/RSII_LambdaSubreads --readType subreads --outType fastq
 
