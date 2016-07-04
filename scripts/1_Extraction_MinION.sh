061f1b6f0e3b09b9a9b241893a436f82a357b09e
Amina Echchiki <amina.echchiki@unil.ch>
2016-07-04 23:10:00 +0200

    section 1: updated paths and tested scripts with student1

--- scripts/1_Extraction_MinION.sh
+++ scripts/1_Extraction_MinION.sh
@@ -9,9 +9,9 @@
 # define working directory for user
 work_dir=/scratch/beegfs/weekly/$USER
 # create working directory for MinION extraction
-mkdir -p $work_dir/lambda_MinION/fast5/
+mkdir -p $work_dir/lambda_minion/fast5/
 # create links to the raw sequencing files
-ln -s /scratch/beegfs/monthly/SIB_long_read_workshop/MinION_lambda_reads/*.fast5 $work_dir/lambda_MinION/fast5/
+ln -s /scratch/beegfs/monthly/SIB_long_read_workshop/MinION_lambda_reads/*.fast5 $work_dir/lambda_minion/fast5/
 
 ## load poretools for read extraction
 module add UHTS/Analysis/poretools/0.5.1 
@@ -19,8 +19,10 @@ module add UHTS/Analysis/poretools/0.5.1
 ## extract reads 
 
 # convert all fast5 reads to fastq
-poretools fastq --type 2D $work_dir/lambda_MinION/fast5/*.fast5 > $work_dir/lambda_MinION/MinION_Lambda2D.fastq
+poretools fastq --type 2D $work_dir/lambda_minion/fast5/*.fast5 > $work_dir/lambda_minion/all_reads.fastq
 
 # get some stats on the raw data using poretools
-poretools stats $work_dir/lambda_MinION/fast5/*.fast5 > $work_dir/lambda_MinION/MinION_Lambda_all_stats.txt
+poretools stats $work_dir/lambda_minion/fast5/*.fast5 > $work_dir/lambda_minion/minion_all_reads_stats.txt
 
+# last tested on: Mon 04 Jul 2016 11:33:07 PM CEST 
+# AE
--- scripts/1_Extraction_RSII.sh
+++ scripts/1_Extraction_RSII.sh
@@ -9,9 +9,9 @@
 # define working directory for user
 work_dir=/scratch/beegfs/weekly/$USER
 # create working directory for MinION extraction
-mkdir -p $work_dir/lambda_RSII/h5/
+mkdir -p $work_dir/lambda_RSII/raw_reads/
 # create links to the raw sequencing files
-ln -s /scratch/beegfs/monthly/SIB_long_read_workshop/RSII_lambda_reads/*.h5 $work_dir/lambda_RSII/h5/
+ln -s /scratch/beegfs/monthly/SIB_long_read_workshop/RSII_lambda_reads/*.h5 $work_dir/lambda_RSII/raw_reads/
 
 ## load pbh5tools for read extraction
 module add UHTS/PacBio/pbh5tools/0.8.0
@@ -19,5 +19,7 @@ module add UHTS/PacBio/pbh5tools/0.8.0
 ## extract reads 
 
 # convert all fast5 reads to fastq
-bash5tools.py $work_dir/lambda_RSII/h5/m140715_200214_42182_c100661412550000001823125411271451_s1_p0.bas.h5 --outFilePrefix $work_dir/lambda_RSII/RSII_LambdaSubreads --readType subreads --outType fastq
+bash5tools.py $work_dir/lambda_RSII/raw_reads/m140715_200214_42182_c100661412550000001823125411271451_s1_p0.bas.h5 --outFilePrefix $work_dir/lambda_RSII/RSII_LambdaSubreads --readType subreads --outType fastq
 
+# last tested on: Mon 04 Jul 2016 11:33:07 PM CEST 
+# AE
